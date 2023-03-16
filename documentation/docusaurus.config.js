96eaca1f774e40ab8fc235d21285339a5a64af37
Cheeze <cheeze@aurastake.com>
2023-03-16 12:07:31 +0800

    Changed the Opengraph card. Updated the Edit URL.

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/auracheeze/experiment1/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/auracheeze/experiment1/blob/main/documentation/",
         },
         blog: false,
         theme: {
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/opengraph-social-card.jpg",
       navbar: {
         title: "Experiment One",
         logo: {
